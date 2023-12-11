7b8aa589b02d5d3f6f802e4330380f6b3969408a
1 <balabanov-03@list.ru>
2023-12-11 23:47:59 +0300

    Rename variable

--- main.go
+++ main.go
@@ -1,78 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"awesomeProject/Data"
+	"encoding/json"
 	httprouter2 "github.com/julienschmidt/httprouter"
-	"io"
 	"log"
 	"net/http"
-	"os/exec"
-	"time"
+	"strconv"
 )
 
 type Response struct {
-	FastestURL string        `json:"fastest_url"`
-	Latency    time.Duration `json:"latency"`
+	UserId int `json:"user_id"`
 }
 
-func main() {
-	//http.HandleFunc("/fastest-mirror", func(writer http.ResponseWriter, request *http.Request) {
-	//	response := FindFastestUrl(Data.MirrorsList)
-	//	responseJSON, _ := json.Marshal(response)
-	//
-	//	writer.Header().Set("Content-Type", "application/json")
-	//	writer.Write(responseJSON)
-	//})
-	//port := ":8000"
-	//server := &http.Server{
-	//	Addr:           port,
-	//	ReadTimeout:    15 * time.Second,
-	//	WriteTimeout:   15 * time.Second,
-	//	MaxHeaderBytes: 1 << 20,
-	//}
-	//fmt.Printf("Starting server on port %s", port)
-	//log.Fatal(server.ListenAndServe())
+var storage Data.Storage = Data.NewMemoryStorage()
 
+func main() {
 	router := httprouter2.New()
 
 	router.ServeFiles("/static/*filepath", http.Dir("C:\\Users\\1\\Desktop"))
+	router.GET("/api/v1/getEmployee", getEmployeeHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func FindFastestUrl(urls []string) Response {
-	urlChan := make(chan string)
-	latencyChan := make(chan time.Duration)
-
-	for _, url := range urls {
-		mirrorURL := url
-		go func() {
-			start := time.Now()
-			_, err := http.Get(mirrorURL + "/README")
-			latency := time.Now().Sub(start) / time.Millisecond
-			if err == nil {
-				urlChan <- mirrorURL
-				latencyChan <- latency
-			}
-		}()
+func getEmployeeHandler(w http.ResponseWriter, r *http.Request, param httprouter2.Params) {
+	val, e := strconv.Atoi(param.ByName("id"))
+	if e != nil {
+		panic(e.Error())
 	}
-	return Response{<-urlChan, <-latencyChan}
-}
 
-func GetFileContent(
-	writer http.ResponseWriter,
-	request *http.Request,
-	params httprouter2.Params) {
-	fmt.Fprintf(writer, GetCommandOutput("/bin/cat", params.ByName("name")))
-}
+	res, err := storage.Get(val)
 
-func GetCommandOutput(command string, arguments ...string) string {
-	out, _ := exec.Command(command, arguments...).Output()
-	return string(out)
-}
+	if err != nil {
+		panic(err.Error())
+	}
 
-func GoVersion(w http.ResponseWriter, r *http.Request, params httprouter2.Params) {
-	response := GetCommandOutput("D:\\GoSDK\\go1.21.5\\bin\\go", "version")
-	io.WriteString(w, response)
-	return
+	jsonString, _ := json.Marshal(Response{UserId: res.Id})
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonString)
 }
